04139a479c469a8e3ed7aef88671b846f99c9701
Matt Logan <mlogan5212@gmail.com>
2020-08-08 11:47:42 -0500

    changed style

--- src/App/App.js
+++ src/App/App.js
@@ -5,9 +5,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Goats</h1>
-        <img src="https://i.imgur.com/1GmEA8Z.jpg" alt="goat"></img>
-        <button className="btn btn-info">I am a button</button>
+        <h1>G.O.A.T</h1>
+        <img src="https://i.imgur.com/mOh9i2R.jpg" alt="goat"></img>
+        <button className="btn btn-secondary">I am a button</button>
       </div>
     );
   }
