f50e876a89c9b4821c7451c1d71ea52cd23e7656
Matt Logan <mlogan5212@gmail.com>
2020-08-10 18:45:18 -0500

    goat data. set state

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+
+import goatData from '../helpers/data/goatData';
 import './App.scss';
 
 class App extends React.Component {
+  state = {
+    goats: [],
+  }
+
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    this.setState({ goats });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>G.O.A.T</h1>
         <img src="https://i.imgur.com/mOh9i2R.jpg" alt="goat"></img>
-        <button className="btn btn-secondary">I am a button</button>
       </div>
     );
   }
