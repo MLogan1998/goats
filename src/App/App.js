0dac679d2e79a59675988ae7ce804ed184ca758f
Matt Logan <mlogan5212@gmail.com>
2020-08-10 20:50:29 -0500

    created goat component

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import GoatCorral from '../components/GoatCorral/GoatCorral';
 import goatData from '../helpers/data/goatData';
 import './App.scss';
 
@@ -14,10 +15,13 @@ class App extends React.Component {
   }
 
   render() {
+    const { goats } = this.state;
+
     return (
       <div className="App">
         <h1>G.O.A.T</h1>
         <img src="https://i.imgur.com/mOh9i2R.jpg" alt="goat"></img>
+        <GoatCorral goats={goats} />
       </div>
     );
   }
