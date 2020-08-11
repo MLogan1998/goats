4793f56551695a34c5da056bf376c0dc72d943a6
Matt Logan <mlogan5212@gmail.com>
2020-08-11 18:44:38 -0500

    added proptype for goat objects

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import goatShape from '../../helpers/propz/goatShape';
+import './Goat.scss';
+
 class Goat extends React.Component {
+  static propTypes = {
+    goat: goatShape.goatShape,
+  }
+
   render() {
     const { goat } = this.props;
     return (
-      <div>
-        <h4>Here is {goat.name}</h4>
+      <div className="card bg-dark text-light border-0">
+        <img className="card-img-top" src={goat.imgUrl} alt="one specific goat" />
+        <div className="card-body">
+          <h5 className="card-title text-uppercase">{goat.name}</h5>
+          <p className="card-text">This goat is {goat.age} years old and is described as: {goat.description}</p>
+        </div>
       </div>
     );
   }
