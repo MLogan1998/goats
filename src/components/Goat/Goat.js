8a1e769b1fccbf81da63b56cc2adab79928aa32f
Matt Logan <mlogan5212@gmail.com>
2020-08-11 20:13:58 -0500

    added take and free goat func to app

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -1,11 +1,25 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import goatShape from '../../helpers/propz/goatShape';
 import './Goat.scss';
 
 class Goat extends React.Component {
   static propTypes = {
     goat: goatShape.goatShape,
+    takeAGoat: PropTypes.func,
+    freeAGoat: PropTypes.func,
+  }
+
+  takeGoatEvent = (e) => {
+    e.preventDefault();
+    const { goat, takeAGoat } = this.props;
+    takeAGoat(goat.id);
+  };
+
+  freeGoatEvent = (e) => {
+    e.preventDefault();
+    const { goat, freeAGoat } = this.props;
+    freeAGoat(goat.id);
   }
 
   render() {
@@ -16,6 +30,15 @@ class Goat extends React.Component {
         <div className="card-body">
           <h5 className="card-title text-uppercase">{goat.name}</h5>
           <p className="card-text">This goat is {goat.age} years old and is described as: {goat.description}</p>
+        </div>
+        <div className="card-footer">
+          {
+            goat.isTaken ? (
+              <button className="btn btn-outline-success" onClick ={this.freeGoatEvent}>free the goat</button>
+            ) : (
+              <button className="btn btn-outline-danger" onClick ={this.takeGoatEvent}>take the goat</button>
+            )
+          }
         </div>
       </div>
     );
